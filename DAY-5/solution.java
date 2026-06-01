ba05e024ab09f2759787323297be881944d4f172
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-06-01 19:12:30 +0530

    Create solution.java

---
+++ DAY-5/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int total=0;
+        int count=0;
+        for(int i=cost.length-1;i>=0;i--){
+            count++;
+            if(count%3!=0){
+                total+=cost[i];
+            }
+        }
+        return total;
+    }
+}
