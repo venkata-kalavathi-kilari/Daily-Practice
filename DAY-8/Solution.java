dbfefd19a3fc2c4845d5f314a96f0a1409ebca55
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-06-04 20:57:14 +0530

    Create Solution.java

---
+++ DAY-8/Solution.java
@@ -0,0 +1,12 @@
+class GfG {
+    static void reverseArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
