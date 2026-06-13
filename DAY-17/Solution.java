29ffbde137912eb167556d199ab98d6ed265dfa7
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-06-13 20:08:53 +0530

    Create Solution.java

---
+++ DAY-17/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int n=arr.length;
+        int maxRight=arr[n-1];
+        ans.add(maxRight);
+        
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>=maxRight){
+                maxRight=arr[i];
+                ans.add(maxRight);
+                
+            }
+        }
+        Collections.reverse(ans);
+        return ;
+    }
+}
