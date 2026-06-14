3754a7d9286f74db9e2393b3dc24136391d50966
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-06-14 19:41:10 +0530

    z B

---
+++ DAY-18/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static ArrayList<Integer> divFloorCeil(int a, int b) {
+        int floorVal = (int) Math.floor((double) a / b);
+        int ceilVal = (int) Math.ceil((double) a / b);
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(floorVal);
+        res.add(ceilVal);
+
+        return res;
+    }
