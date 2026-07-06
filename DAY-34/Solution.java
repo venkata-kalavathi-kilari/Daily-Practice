a8d5ac90508238ea36bf2f939b5c31f4736fd784
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-07-06 21:23:40 +0530

    Add string comparison function to Solution.java

---
+++ DAY-34/Solution.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class GfG {
+    
+    // Function to compare two strings using equals
+    public static boolean areStringsSame(String s1, String s2) {
+        return s1.equals(s2);
+   }
