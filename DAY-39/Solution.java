da17860db6ff45b6eac9c6d3ed4449669f09833f
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-07-18 22:11:59 +0530

    Create Solution.java

---
+++ DAY-39/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    int upperBound(int[] arr, int target) {
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]>target){
+                return i;
+            }
+        }
+        return n;
+    }
+}
