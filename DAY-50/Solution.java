9498a4451caa9fb817f8dbcb432cfe4ed7572a24
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-07-30 21:04:24 +0530

    Create Solution.java

---
+++ DAY-50/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int minimumPushes(String word) {
+        int ans=0;
+        int n = word.length();
+        for(int i=0;i<n;i++){
+            ans += (i / 8) + 1;
+            
+        }
+        
+         return ans;
+    }
+}
