6edc654a10ebea47a6c0b103b60bfc122d0d1261
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-08-06 21:27:25 +0530

    Create Solution.java

---
+++ DAY-57/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(true){
+        int temp=n;
+        int product=1;
+        while(temp>0){
+            int digit=temp%10;
+            product*=digit;
+            temp/=10;
+        }
+        if(product%t==0){
+            return n;
+        }
+        n++;
+        }
+    }
+}
