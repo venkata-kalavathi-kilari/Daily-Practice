64bfa192bc2c7b0f452501f0d2ee2c20b594f260
Venkatan Kalavathi Kilari <venkatakalavathikilari@gmail.com>
2026-08-18 11:05:01 +0530

    Create Solution3.java

---
+++ DAY-69/Solution3.java
@@ -0,0 +1,24 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean detectLoop(Node head) {
+        // code here
+        Node slow=head,fast=head;
+        while(slow!=null &&fast !=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
